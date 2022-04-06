ac8302ef17ae03182d914a993bc17235a41dfdd4
Orkney06 <kome8728@gmail.com>
2022-04-06 13:27:19 +0900

    add routing

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.db import router
 from django.urls import path, include
 from rest_framework import routers
 from todo import views
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
@@ -10,4 +11,5 @@ router.register(r'todos', views.TodoView, 'todo')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
